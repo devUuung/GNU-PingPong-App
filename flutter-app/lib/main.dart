fcf1234f920ee9300fb611bd80d6e02cd04daaa2
dev_ung <87903147+devUuung@users.noreply.github.com>
2025-03-10 15:19:13 +0900

    GitHub Pages 배포 워크플로우 추가

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,5 +1,6 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_app/providers/games_info_provider.dart';
 import 'package:flutter_app/providers/users_info_provider.dart';
@@ -8,14 +9,20 @@ import 'package:flutter_app/screens/login.dart';
 import 'package:flutter_app/screens/post_create.dart';
 import 'package:flutter_app/screens/home.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() async {
   // Flutter 엔진이 위젯을 바인딩하기 전에 플러그인 초기화 보장
   WidgetsFlutterBinding.ensureInitialized();
 
   // 블루투스 플러그인 초기화
-  await FlutterBluetoothSerial.instance;
+  // await FlutterBluetoothSerial.instance;
+
+  // 앱의 화면 방향을 세로 모드로 고정 (상하 반전 포함)
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   initializeDateFormatting('ko_KR');
 
