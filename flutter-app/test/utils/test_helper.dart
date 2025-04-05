f6fb753cf0dc3e0154af32a5ff084004796ddd6e
dev_ung <87903147+devUuung@users.noreply.github.com>
2025-04-05 16:13:57 +0900

    test: shared preference init

--- flutter-app/test/utils/test_helper.dart
+++ flutter-app/test/utils/test_helper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> setupTestEnvironment() async {
   // Flutter 엔진 초기화
@@ -9,6 +10,9 @@ Future<void> setupTestEnvironment() async {
 
   // .env 파일 로드
   await dotenv.load(fileName: '.env');
+
+  // shared_preferences 초기화
+  SharedPreferences.setMockInitialValues({});
 
   // Supabase 초기화
   const supabaseUrl = 'https://neyijpnwimzgeszwupzh.supabase.co';
